fb5203f75755a2ea3bb342fe9416f3acc167347a
mourdorigo <rodrigobt20@gmail.com>
2014-02-06 13:13:07 -0200

    posicionamento dos botões para ambos os players com compatibilidade para todos os equipamentos.

--- qoda/MyScene.h
+++ qoda/MyScene.h
@@ -7,7 +7,19 @@
 //
 
 #import <SpriteKit/SpriteKit.h>
+#import "SKButton.h"
 
 @interface MyScene : SKScene
 
+@property (nonatomic, retain) SKButton *btn1P1;
+@property (nonatomic, retain) SKButton *btn2P1;
+@property (nonatomic, retain) SKButton *btn3P1;
+@property (nonatomic, retain) SKButton *btn4P1;
+
+@property (nonatomic, retain) SKButton *btn1P2;
+@property (nonatomic, retain) SKButton *btn2P2;
+@property (nonatomic, retain) SKButton *btn3P2;
+@property (nonatomic, retain) SKButton *btn4P2;
+
 @end
+
--- qoda/MyScene.m
+++ qoda/MyScene.m
@@ -7,16 +7,17 @@
 //
 
 #import "MyScene.h"
-#import "SKButton.h"
 
 #define btns 4
+#define btnSizeScale 10
 @implementation MyScene{
     NSMutableArray *arrBtnsP1;
     NSMutableArray *arrBtnsP2;
     
 }
+@synthesize btn1P1, btn1P2, btn2P1, btn2P2, btn3P1, btn3P2, btn4P1, btn4P2;
 
--(id)initWithSize:(CGSize)size {    
+-(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
@@ -54,39 +55,110 @@
     /* Called right after the view shows the scene */
     
     arrBtnsP1 = [[NSMutableArray alloc]initWithCapacity:0];
-    
-    for (int h=1; h<=btns; h++) {
-        /*
-        SKSpriteNode *btnp = [SKSpriteNode spriteNodeWithImageNamed:@"buttonRed.png"];
-        [self addChild:btnp];
-        btnp.position = [self getPointFromScaleX:((100/btns/2)*h)+5 y:98];
-        btnp.size = CGSizeMake(self.view.frame.size.width/15, self.view.frame.size.width/15);
-        [arrBtnsP1 addObject:btnp];
-    */
-        SKButton *backButton = [[SKButton alloc] initWithImageNamedNormal:@"buttonRed.png" selected:@"buttonPressed.png"];
-        [backButton setPosition:[self getPointFromScaleX:((100/btns/2)*h)+5 y:98]];
-        [backButton.title setText:@"Button"];
-        [backButton.title setFontName:@"Chalkduster"];
-        [backButton.title setFontSize:20.0];
-        [backButton setTouchUpInsideTarget:self action:@selector(actionBtn)];
-        [self addChild:backButton];
-         }
-    
-    for (int h=1; h<=btns; h++) {
-        SKSpriteNode *btnp = [SKSpriteNode spriteNodeWithImageNamed:@"buttonGreen.png"];
+
+    for (int p = 1; p<=2; p++) {
+        int posy;
+        if (p==1) {
+            posy = 20;
+        }else{
+            posy = 80;
+        }
         
-        [self addChild:btnp];
-        btnp.position = [self getPointFromScaleX:((100/btns/2)*h)+5 y:35];
-        btnp.size = CGSizeMake(self.view.frame.size.width/15, self.view.frame.size.width/15);
-        [arrBtnsP2 addObject:btnp];
-        
+        for (int b=1; b<=btns; b++) {
+            SKButton *tempButton = [[SKButton alloc] initWithImageNamedNormal:@"buttonRed.png" selected:@"buttonPressed.png"];
+            [tempButton setSize:CGSizeMake(self.view.frame.size.width/btnSizeScale, self.view.frame.size.width/btnSizeScale)];
+            [tempButton setPosition:[self getPointFromScaleX:((100/btns/1.2)*b) y:posy]];
+            
+            switch (b) {
+                case 1:
+                    if (p==1) {
+                        [tempButton setTouchUpInsideTarget:self action:@selector(actionBtn1P1)];
+                        btn1P1 = tempButton;
+                    }else{
+                        [tempButton setTouchUpInsideTarget:self action:@selector(actionBtn1P2)];
+                        btn1P2 = tempButton;
+                    }
+                    
+                    break;
+                case 2:
+                    if (p==1) {
+                        [tempButton setTouchUpInsideTarget:self action:@selector(actionBtn2P1)];
+                        btn2P1 = tempButton;
+                    }else{
+                        [tempButton setTouchUpInsideTarget:self action:@selector(actionBtn2P2)];
+                        btn2P2 = tempButton;
+                    }
+                    
+                    break;
+                case 3:
+                    if (p==1) {
+                        [tempButton setTouchUpInsideTarget:self action:@selector(actionBtn3P1)];
+                        btn3P1 = tempButton;
+                    }else{
+                        [tempButton setTouchUpInsideTarget:self action:@selector(actionBtn3P2)];
+                        btn3P2 = tempButton;
+                    }
+                    
+                    break;
+                case 4:
+                    if (p==1) {
+                        [tempButton setTouchUpInsideTarget:self action:@selector(actionBtn4P1)];
+                        btn4P1 = tempButton;
+                    }else{
+                        [tempButton setTouchUpInsideTarget:self action:@selector(actionBtn4P2)];
+                        btn4P2 = tempButton;
+                    }
+                    
+                    break;
+            }
+
+            
+        }
     }
+    
+    [self addChild:btn1P1];
+    [self addChild:btn2P1];
+    [self addChild:btn3P1];
+    [self addChild:btn4P1];
+    [self addChild:btn1P2];
+    [self addChild:btn2P2];
+    [self addChild:btn3P2];
+    [self addChild:btn4P2];
     
 }
 
--(IBAction)actionBtn{
-    NSLog(@"ACTION!");
+-(IBAction)actionBtn1P1{
+    NSLog(@"actionBtn1P1!");
 }
+
+-(IBAction)actionBtn2P1{
+    NSLog(@"actionBtn2P1!");
+}
+
+-(IBAction)actionBtn3P1{
+    NSLog(@"actionBtn3P1!");
+}
+
+-(IBAction)actionBtn4P1{
+    NSLog(@"actionBtn4P1!");
+}
+
+-(IBAction)actionBtn1P2{
+    NSLog(@"actionBtn1P2!");
+}
+
+-(IBAction)actionBtn2P2{
+    NSLog(@"actionBtn2P2!");
+}
+
+-(IBAction)actionBtn3P2{
+    NSLog(@"actionBtn3P2!");
+}
+
+-(IBAction)actionBtn4P2{
+    NSLog(@"actionBtn4P2!");
+}
+
 
 -(CGPoint)getPointFromScaleX:(float)x y:(float)y{
     NSLog(@"return view %f %f ", (self.view.frame.size.height/100)*x, (self.view.frame.size.width/100)*y);
